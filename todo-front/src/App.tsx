d4da81d53b9025b1353eed4746d816c1ce264851
Vinicius Murara <viniciusvieiramurara@gmail.com>
2025-02-13 08:13:39 -0300

    criando o front em react com o vite

---
+++ todo-front/src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <h1 className="text-red-500 text-8xl">Text</h1>
+    </div>
+  )
+}
+
+export default App
